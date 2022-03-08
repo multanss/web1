8b4831b51da0809069a390a95125b9f5382da5ca
multanss <90046815+multanss@users.noreply.github.com>
2022-03-09 01:40:23 +0900

    Delete pratice.py

--- pratice.py
+++
@@ -1,7 +0,0 @@
-from random import *
-print((int(random() *45))+1,(int(random() *45))+1,(int(random() *45))+1,(int(random() *45))+1,(int(random() *45))+1,(int(random() *45))+1)
-
-print(randrange(1,46),randrange(1,46),randrange(1,46),randrange(1,46),randrange(1,46),randrange(1,46))
-
-print(randint(1,45),randint(1,45),randint(1,45),randint(1,45),randint(1,45),randint(1,45))
-
